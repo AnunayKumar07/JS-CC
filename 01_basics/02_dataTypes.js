48f52ada7341a2908bfedea816bcffbd2471fc61
Anunay Kumar <kumaranunay76@gmail.com>
2025-10-08 18:45:40 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3)  //we r using nodeJS , not browser
+
+console.log(3+3)  //code readibility should be high
+console.log("Anunay")
+
+
+let name="anunay"
+let age=18
+let isLoggedIn=false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => uniqueness
+
+// object 
+
+console.log(typeof "anunay");
+console.log(typeof age);
+console.log(typeof null);  //null is object
+console.log(typeof undefined);  //undefined
